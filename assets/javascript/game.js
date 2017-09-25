33463f0e0772615f4eea48a92f845f195274d8e0
Legion <xdjlee@gmail.com>
2017-09-25 11:43:10 -0700

    update initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// vars target crys1 crys2 crys3 crys4 score
+// obj. keys-> wins losses
+// reset()
+// score 0
+// new target (ceil random*102) + 18
+// rerandom crystal value ceil random * 12
+// updatewinloss()
+// update wins
+// update losses
+// on click
+// add value to score
+// if score < t update score
+// if score = t obj.wins++ reset()
+// if score > t obj.losses++ reset()
